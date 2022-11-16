3d9fd72f1db85fb74315c657cc21dff91fdc49de
charartesani <charlotteartesani@gmail.com>
2022-11-15 20:27:48 -0500

    doggos

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
 
-// Select some elements...
+}
+let footer = document.querySelector('.footer')
+footer.style.textAlign = "left"
+footer.style.borderStyle = 'solid'
+// Select some elements...s
